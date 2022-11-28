2c76282ba42b44a3157e792cf5269b9c8deaaffe
GOlivierNation <ogasominali@gmail.com>
2022-11-28 14:42:36 -0500

    Configure environment database and add missing dependencies

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,19 +4,15 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 
-
-
 class HomepageController extends AppController
 
 {
     [Route('/', name: 'homepage')]
-    public function index(): Response
+        public function index(): Response
     {
-        return $this->render(view:'homepage/index.html.twig', [
+        return $this->render(view('homepage/index.html.twig'));
             'controller_name' => 'HomepageController',
-        ]);
     }
 }
-
 
 ?>
